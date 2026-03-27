65a87da4c1798112b96f9b517777aa47d7a5a23e
Chacko Martin <24markjr@gmail.com>
2026-03-27 12:17:03 +0530

    Add method for integer addition in App class
    Added a method 'add' to perform addition of two integers.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    // Added method for testing (IMPORTANT for coverage)
+    public int add(int a, int b) {
+        return a + b;
     }
 }
